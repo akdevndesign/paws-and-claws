3bfd95e6917d504e44031624b55181ffa8512797
alexoserna <118146045+alexoserna@users.noreply.github.com>
2023-05-10 19:08:09 -0400

    recent build:

--- client/src/pages/PetProfile.js
+++ client/src/pages/PetProfile.js
@@ -76,7 +76,7 @@ export default function PetProfile() {
                         </div>
                     </div>
                 </div>
-                <div className="row justify-content-center pt-5">
+                <div className="row justify-content-center">
                     <div className="col-md-4 text-center pt-5">
                         <button className="btn btn-lg" id="adoptNow">Adopt Me Now!</button>
                     </div>
